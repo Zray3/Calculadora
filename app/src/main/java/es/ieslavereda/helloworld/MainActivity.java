484c4a64800794aec7bea357ee72000fe5a419d5
Carlos <carloscremadesaleixandre@gmail.com>
2023-04-23 16:16:06 +0200

    2.0

--- app/src/main/java/es/ieslavereda/helloworld/MainActivity.java
+++ app/src/main/java/es/ieslavereda/helloworld/MainActivity.java
@@ -5,18 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView display;
-    private Button buttonClear;
-    private Button buttonPlus;
-    private  Button buttonMinus;
-    private  Button buttonMultiply;
-    private Button buttonDividir;
-    private Button buttonEquals;
+    private Button buttonClear, buttonPlus, buttonMinus, buttonMultiply, buttonDividir,buttonEquals,buttonAns;
+    private CheckBox opciones;
+    private RadioButton division, multi, suma, resta;
+    private double ans;
+    private RadioGroup radioGroup;
+
     private double operando;
     private Operador operador;
+    private boolean checkedCB=false;
+
+    private boolean[] checkedArr = new boolean[4];
 
 
     @Override
@@ -30,30 +37,50 @@ public class MainActivity extends AppCompatActivity {
         buttonMinus = findViewById(R.id.buttonMinus);
         buttonMultiply = findViewById(R.id.buttonMultiply);
         buttonDividir = findViewById(R.id.buttonDivide);
+        buttonAns = findViewById(R.id.buttonAns);
+        opciones = findViewById(R.id.Opciones);
+        suma=findViewById(R.id.suma);
+        resta=findViewById(R.id.resta);
+        division=findViewById(R.id.division);
+        multi=findViewById(R.id.multi);
+        radioGroup=findViewById(R.id.operaciones);
 
         buttonClear.setOnClickListener(view -> display.setText("0"));
+
         buttonPlus.setOnClickListener(view -> {
+            if (checkedArr[0]==false){
             operando = Double.parseDouble(display.getText().toString());
             operador = Operador.SUMA;
-            display.setText("0");
+            display.setText("0");}
 
         });
         buttonMinus.setOnClickListener(view -> {
+            if (checkedArr[1]==false){
             operando = Double.parseDouble(display.getText().toString());
             operador = Operador.RESTA;
             display.setText("0");
+            }
 
         });
         buttonMultiply.setOnClickListener(view -> {
-            operando = Double.parseDouble(display.getText().toString());
-            operador = Operador.MULTIPLICACION;
-            display.setText("0");
+            if (checkedArr[2]==false) {
+                operando = Double.parseDouble(display.getText().toString());
+                operador = Operador.MULTIPLICACION;
+                display.setText("0");
+            }
 
         });
         buttonDividir.setOnClickListener(view -> {
+            if (checkedArr[3]==false){
             operando = Double.parseDouble(display.getText().toString());
             operador = Operador.DIVISION;
             display.setText("0");
+            }
+
+        });
+
+        buttonAns.setOnClickListener(view -> {
+           display.setText(String.valueOf(ans));
 
         });
 
@@ -78,17 +105,60 @@ public class MainActivity extends AppCompatActivity {
                         Double.parseDouble(display.getText().toString());
                 display.setText(resultado.toString());
             }
+            ans=Double.parseDouble(display.getText().toString());
         });
+
+
+    }
+    public void cheked(View view){
+        checkedCB=!checkedCB;
+        if (checkedCB==false){
+            radioGroup.setVisibility(View.INVISIBLE);
+            radioGroup.clearCheck();
+            for (int i=0;checkedArr.length>i; i++){
+                checkedArr[i]=false;
+            }
+        }else {
+            radioGroup.setVisibility(View.VISIBLE);
+        }
     }
 
 
-
     public void onClick(View view){
-        if (display.getText().equals("0")){
-            display.setText(((Button) view).getText());
+
+        if (view.getId()==suma.getId()||view.getId()==resta.getId()||view.getId()==multi.getId()||view.getId()==division.getId()){
+            if (view.getId()==suma.getId()){
+                checkedArr[0]=true;
+            }else {
+                checkedArr[0]=false;
+            }
+
+            if (view.getId()==resta.getId()){
+                checkedArr[1]=true;
+            }else {
+                checkedArr[1]=false;
+            }
+
+            if (view.getId()==multi.getId()){
+                checkedArr[2]=true;
+            }else {
+                checkedArr[2]=false;
+            }
+
+            if (view.getId()==division.getId()){
+                checkedArr[3]=true;
+            }else {
+                checkedArr[3]=false;
+            }
         }else {
-            display.setText(display.getText().toString()+((Button) view).getText());
+
+            if (display.getText().equals("0")) {
+                display.setText(((Button) view).getText());
+            } else {
+                display.setText(display.getText().toString() + ((Button) view).getText());
+            }
         }
+
     }
 
 }
